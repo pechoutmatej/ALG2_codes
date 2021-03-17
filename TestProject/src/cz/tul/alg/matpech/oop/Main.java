7f27426e17788f02c88af76a795d98f99a589aab
pechoutmatej <matej.pechout@tul.cz>
2021-03-17 14:28:03 +0100

    Add TestProject

---
+++ TestProject/src/cz/tul/alg/matpech/oop/Main.java
@@ -0,0 +1,9 @@
+package cz.tul.alg.matpech.oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Git");
+    }
+}
